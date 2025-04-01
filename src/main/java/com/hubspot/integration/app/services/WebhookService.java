477086a8679ecc88c5e825ee604c3fb114fb9c0b
Anderson Oliveira <andervilo@gmail.com>
2025-04-01 14:47:57 -0300

    Add structured logging for incoming webhooks
    Replaced unstructured logging with a standardized format to improve log readability and consistency. Removed an unused import and cleaned up redundant code for better maintainability.

--- src/main/java/com/hubspot/integration/app/services/WebhookService.java
+++ src/main/java/com/hubspot/integration/app/services/WebhookService.java
@@ -2,7 +2,6 @@ package com.hubspot.integration.app.services;
 
 import com.hubspot.integration.domain.entities.HubspotEvent;
 import com.hubspot.integration.infra.repositories.HubspotEventRepository;
-import com.hubspot.integration.infra.repositories.HubspotTokenRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -14,8 +13,8 @@ public class WebhookService {
     private final HubspotEventRepository eventRepository;
 
     public void processWebhook(final String payload) {
+        log.info("[WEBHOOK RECEIVED]: {}", payload);
         var hubspotEvent = HubspotEvent.of(payload);
         eventRepository.save(hubspotEvent);
-        log.info("Recebido webhook: {}", payload);;
     }
 }
